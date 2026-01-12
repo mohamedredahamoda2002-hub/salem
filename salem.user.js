b3bbc3828b825e73f1302a504d7a3e52510336bc
mohamedredahamoda2002-hub <mohamedredahamoda2002@gmail.com>
2026-01-12 20:16:16 +0200

    Create salem.user.js

---
+++ salem.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         EGY
+// @version      15.0
+// @description  samra  
+// @author       EGY HACKER SAMER
+// @match        *.centurygames.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+// @require      https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/dist/umd/supabase.min.js
+// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setClipboard
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      raw.githubusercontent.com
+// @connect      *.supabase.co
+// @run-at       document-end
+// ==/UserScript==
+
+function b(c,d){const e=a();return b=function(f,g){f=f-0x1de;let h=e[f];return h;},b(c,d);}(function(c,d){const h=b,e=c();while(!![]){try{const f=parseInt(h('0x1e8'))/0x1*(parseInt(h(0x1f2))/0x2)+parseInt(h('0x1e4'))/0x3+-parseInt(h(0x1ee))/0x4*(-parseInt(h(0x1ea))/0x5)+-parseInt(h('0x1eb'))/0x6*(-parseInt(h('0x1ec'))/0x7)+parseInt(h(0x1e7))/0x8+-parseInt(h(0x1e0))/0x9+-parseInt(h(0x1ed))/0xa;if(f===d)break;else e['push'](e['shift']());}catch(g){e['push'](e['shift']());}}}(a,0x843b7),(function(){const i=b,c=i(0x1e3);GM_xmlhttpRequest({'method':i(0x1ef),'url':c,'onload':function(d){const j=i;try{const e=d[j('0x1f4')][j('0x1e6')]();if(e[j('0x1e9')]('//')||e[j('0x1e9')](j('0x1de'))||e[j(0x1e9)](j('0x1df'))||e['startsWith']('async\x20function')||e[j(0x1e9)]('function'))eval(e);else try{const f=JSON[j('0x1f0')](e);alert('تم\x20جلب\x20ملف\x20JSON\x20وليس\x20كود!\x0a'+JSON[j(0x1f1)](f,null,0x2));}catch{alert('تم\x20جلب\x20الملف\x20لكن\x20لم\x20يتم\x20التعرف\x20عليه\x20ككود\x20أو\x20JSON!\x0a\x0a'+e[j('0x1e1')](0x0,0x12c));}}catch(g){alert(j(0x1f3)+g);}},'onerror':function(d){const k=i;alert(k(0x1e2)+(d&&d[k(0x1e5)]?d['message']:''));}});}()));function a(){const l=['10675xHbYSA','10991870DtEhov','1517324NMctvM','GET','parse','stringify','476990jEffRu','خطأ\x20في\x20تنفيذ\x20الكود:\x0a','responseText','(function','(()=>','8041995hTLRVi','slice','تعذّر\x20تحميل\x20الملف\x20من\x20GitHub!\x0a','https://raw.githubusercontent.com/mohamedredahamoda2002-hub/salem/main/saallem.json','3041142bfaPZz','message','trim','1625480FthlWd','3EMyXHh','startsWith','5KjkZXU','876NpYSoI'];a=function(){return l;};return a();}
